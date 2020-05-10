d98ecf481068929f99b1c2f3737876b2e81c2094
Elom Atsou Agboka <opentech.consult@gmail.com>
2020-05-10 02:29:37 +0000

    Weather-app
    Demonstrate the Asynchronous executions of instructions using node setTimeOut()
    That method takes two arguments. The First is a callback function, and the second is a delay that set time after which the function will be called.
    This setTimeout() does not block the flow of execution, while setTimeout is waiting for the delay to expire, the rest of the instructions of the program is executing.

--- playground/async-basics.js
+++ playground/async-basics.js
@@ -2,4 +2,12 @@
 // console.log() methods that execute synchronously
 console.log('Starting App');
 
+setTimeout(() => {
+    console.log('Inside of callback');
+}, 3000);
+
+setTimeout(() => {
+    console.log('Second setTimeout works!');
+}, 0);
+
 console.log('Finishing Up');
